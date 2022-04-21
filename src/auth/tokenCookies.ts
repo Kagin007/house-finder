8b50335f5cfb269c842941a7b2173b7d8bbfbf4a
Kagin007 <adamschulte148@hotmail.com>
2022-04-21 09:51:04 -0400

    add auth changes

--- src/auth/tokenCookies.ts
+++ src/auth/tokenCookies.ts
@@ -1,19 +1,32 @@
-export const setTokenCookie = (token: string) => {
-  fetch("/api/login", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ token }),
-  });
-};
+import cookies from "js-cookie";
 
-export const removeTokenCookie = () => {
-  fetch("/api/logout", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  });
-};
+export const getTokenCookie = () => cookies.get("token");
+
+export const setTokenCookie = (token: string) => {
+  cookies.set("token", token, {
+    expires: 1 / 24,
+  })
+  
+}
+
+export const removeTokenCookie = () => cookies.remove("token")
+
+// export const setTokenCookie = (token: string) => {
+//   fetch("/api/login", {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ token }),
+//   });
+// };
+
+// export const removeTokenCookie = () => {
+//   fetch("/api/logout", {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({}),
+//   });
+// };
